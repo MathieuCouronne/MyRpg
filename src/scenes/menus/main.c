ae1a2b7c4a8a213b34da6bf5490306d850c6b70a
Mathis Le Bonniec <mathis.le-bonniec@epitech.eu>
2022-04-08 12:00:36 +0200

    feat(display): check every pointers before using them

--- src/scenes/menus/main.c
+++ src/scenes/menus/main.c
@@ -11,12 +11,16 @@
 
 bool display_main_menu(game_t *game)
 {
-    sfRenderWindow *window = game->window;
-    main_menu_scenes_t *menu = game->scenes->main_menu;
-    menu_buttons_t *buttons = menu->buttons;
+    sfRenderWindow *window = NULL;
+    main_menu_scenes_t *menu = NULL;
+    menu_buttons_t *buttons = NULL;
 
-    if (!menu)
+    if (!game || !game->window || !game->scenes || !game->scenes->main_menu ||
+    !game->scenes->main_menu->buttons)
         return false;
+    window = game->window;
+    menu = game->scenes->main_menu;
+    buttons = menu->buttons;
     sfRenderWindow_drawSprite(window, menu->background->sprite, NULL);
     sfRenderWindow_drawSprite(window, buttons->play->asset->sprite, NULL);
     sfRenderWindow_drawText(window, buttons->play->text, NULL);

ebea60358494e09799ae4b2e3bc0897250ca5b46
Mathis Le Bonniec <mathis.le-bonniec@epitech.eu>
2022-04-08 11:51:04 +0200

    feat(display): add function to display main menu

--- src/scenes/menus/main.c
+++ src/scenes/menus/main.c
@@ -5,13 +5,22 @@
 ** Game arrow keys events handling
 */
 
+#include <stdbool.h>
 #include "my_rpg.h"
+#include "structs.h"
 
-void handle_arrow_keys(game_t *game, sfEvent *event)
+bool display_main_menu(game_t *game)
 {
-    if (event->key.code == sfKeyLeft) {
-        sfView_move(game->view, (sfVector2f) {-1, 0});
-    } else if (event->key.code == sfKeyRight) {
-        sfView_move(game->view, (sfVector2f) {1, 0});
-    }
+    sfRenderWindow *window = game->window;
+    main_menu_scenes_t *menu = game->scenes->main_menu;
+    menu_buttons_t *buttons = menu->buttons;
+
+    if (!menu)
+        return false;
+    sfRenderWindow_drawSprite(window, menu->background->sprite, NULL);
+    sfRenderWindow_drawSprite(window, buttons->play->asset->sprite, NULL);
+    sfRenderWindow_drawText(window, buttons->play->text, NULL);
+    sfRenderWindow_drawSprite(window, buttons->settings->asset->sprite, NULL);
+    sfRenderWindow_drawText(window, buttons->settings->text, NULL);
+    return true;
 }

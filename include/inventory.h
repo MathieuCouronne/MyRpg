09c77a21981d0f1333886a3bbc71fc47743ca948
jtual <joshua.tual@epitech.eu>
2022-04-25 13:30:36 +0200

    feat(inventory): added header + creates and fill empty inventory

---
+++ include/inventory.h
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2021
+** structs.h
+** File description:
+** my_rpg structs header file
+*/
+
+#include <SFML/Graphics.h>
+
+#define INVENTORY_HEIGHT 4
+#define INVENTORY_WIDTH 9
+
+#ifndef INVENTORY_H_
+    #define INVENTORY_H_
+
+typedef struct slot_s {
+    char *name;
+    sfVector2f pos;
+    unsigned int quantity;
+} slot_t;
+
+typedef struct inventory_s {
+    unsigned int height;
+    unsigned int width;
+    slot_t **content;
+} inventory_t;
+
+#endif

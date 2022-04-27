49873bd292835429f476a2e6b119734b9b37398b
jtual <joshua.tual@epitech.eu>
2022-04-27 19:46:01 +0200

    feat(character): added maccros for stats

--- include/character.h
+++ include/character.h
@@ -8,6 +8,21 @@
 #ifndef CHARACTER_H_
     #define CHARACTER_H_
 
+    #define NB_STATS 5
+    #define NB_CLASSES 2
+
+    #define BASE_WAR_STR 10
+    #define BASE_WAR_DEF 5
+    #define BASE_WAR_VIT 100
+    #define BASE_WAR_SPD 100
+    #define BASE_WAR_DEX 5
+
+    #define BASE_MAGE_STR 5
+    #define BASE_MAGE_DEF 3
+    #define BASE_MAGE_VIT 75
+    #define BASE_MAGE_DEX 10
+    #define BASE_MAGE_SPD 100
+
 typedef struct stats_s {
     char *name;
     unsigned int amount;

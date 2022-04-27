9208695f7d6cf2b9ea72e69e844199f0451d4422
Mathis Le Bonniec <mathis.le-bonniec@epitech.eu>
2022-04-27 17:50:33 +0200

    fix(assets): download all assets before using it

--- src/my_rpg.c
+++ src/my_rpg.c
@@ -28,7 +28,6 @@ int main(void)
 
     if (!game)
         return 84;
-    download_assets();
     while (sfRenderWindow_isOpen(game->window)) {
         sfRenderWindow_clear(game->window, sfBlack);
         event_handling(game, &event);

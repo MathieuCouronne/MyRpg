4651683eb574668b12da55f6b8887f38ccbbf273
jtual <joshua.tual@epitech.eu>
2022-04-27 17:30:02 +0200

    feat(character): added character's structure

---
+++ include/character.h
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2021
+** my_rpg.h
+** File description:
+** my_rpg header file
+*/
+
+#ifndef CHARACTER_H_
+    #define CHARACTER_H_
+
+typedef struct stats_s {
+    char *name;
+    unsigned int amount;
+} stats_t;
+
+typedef struct character_s {
+    char *name;
+    char *class_name;
+    unsigned int unspent;
+    stats_t **stats;
+} character_t;
+
+#endif

deb08e683a577a71124f2001f2b8467a5e7e3628
jtual <joshua.tual@epitech.eu>
2022-04-27 19:46:33 +0200

    feat(character): initializes base stats as warrior

--- src/character/create_character.c
+++ src/character/create_character.c
@@ -14,9 +14,44 @@
 #include "inventory.h"
 #include "character.h"
 
+static stats_t **init_war_stats(void)
+{
+    stats_t **stats = malloc(sizeof(stats_t *) * NB_STATS + 1);
+
+    stats[NB_STATS] = NULL;
+    stats[0]->name = "STR";
+    stats[0]->amount = BASE_WAR_STR;
+    stats[1]->name = "DEF";
+    stats[1]->amount = BASE_WAR_DEF;
+    stats[2]->name = "VIT";
+    stats[2]->amount = BASE_WAR_VIT;
+    stats[3]->name = "SPD";
+    stats[3]->amount = BASE_WAR_SPD;
+    stats[4]->name = "DEX";
+    stats[4]->amount = BASE_WAR_DEX;
+    return stats;
+}
+
+static char **init_classes(void)
+{
+    char **classes = malloc(sizeof(char *) * NB_CLASSES + 1);
+
+    classes[NB_CLASSES] = NULL;
+    classes[0] = "Warrior";
+    classes[1] = "Mage";
+    return classes;
+}
+
 character_t *create_character(void)
 {
-    character_t *character = NULL;
+    character_t *character = malloc(sizeof(character_t *));
 
+    character->name = malloc(sizeof(char) * 30);
+    character->name = "Enter a name";
+    character->class_name = init_classes()[0];
+    character->unspent = 20;
+    character->stats = init_war_stats();
+    character->level = 1;
+    character->exp = 0;
     return character;
 }

f63a4b68fcf6516f5f7975c833966054dbc541bd
Mathis Le Bonniec <mathis.le-bonniec@epitech.eu>
2022-04-29 09:50:35 +0200

    tests(style): add check to test if filename is in lowercase

--- tests/style.py
+++ tests/style.py
@@ -19,6 +19,8 @@ def manage_comments(name, lines):
             in_c['c'].append(match.pos)
         elif match and name[-1] == 'h' and (line[match.span()[1]] != ' ' or (line[match.span()[1]] == ' ' and line[match.span()[1] + 1].islower())):
             in_c['h'].append(match.pos)
+    if not name.islower():
+        print(f"File {name} is not in lowercase.")
     if len(in_c['c']) != 0:
         amount = len(in_c['c'])
         print(f"{amount} comments {'has' if amount == 1 else 'have'} been detected in {name}")

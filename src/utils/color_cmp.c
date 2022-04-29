ef1ad6e87a3209b4dcfd8d60a6cfc260c59a238f
Mathis Le Bonniec <mathis.le-bonniec@epitech.eu>
2022-04-29 17:55:36 +0200

    feat(utils): add utils function to compare two sfColor

--- src/utils/color_cmp.c
+++ src/utils/color_cmp.c
@@ -10,9 +10,6 @@
 
 bool color_cmp(sfColor color1, sfColor color2)
 {
-
-    printf("1 -> r: %d, g: %d, b: %d\n", color1.r, color1.g, color1.b);
-    printf("2 -> r: %d, g: %d, b: %d\n\n", color2.r, color2.g, color2.b);
     if (
         color1.r == color2.r &&
         color1.g == color2.g &&

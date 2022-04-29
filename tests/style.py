d4357c423870d849e4e60fa3cfa4dda5277daae2
Mathis Le Bonniec <mathis.le-bonniec@epitech.eu>
2022-04-29 10:33:25 +0200

    tests(style): check if files are containings pathes

--- tests/style.py
+++ tests/style.py
@@ -12,25 +12,33 @@ for directory in directories:
 
 
 def manage_comments(name, lines):
-    in_c = {'c': [], 'h': []}
-    for line in lines:
-        match = search("\/\/", line.rstrip())
-        if match and name[-1] == 'c':
-            in_c['c'].append(match.pos)
-        elif match and name[-1] == 'h' and (line[match.span()[1]] != ' ' or (line[match.span()[1]] == ' ' and line[match.span()[1] + 1].islower())):
-            in_c['h'].append(match.pos)
+    errors = {'c': [], 'h': [], 'pathes': []}
+    for i, line in enumerate(lines):
+        match_comments = search("\/\/", line)
+        match_magic_pathes = search("\.\/.*", line)
+        if match_magic_pathes and name[-1] != 'h':
+            errors['pathes'].append(i)
+        if match_comments and name[-1] == 'c':
+            errors['c'].append(i)
+        elif match_comments and name[-1] == 'h' and (line[match_comments.span()[1]] != ' ' or (line[match_comments.span()[1]] == ' ' and line[match_comments.span()[1] + 1].islower())):
+            errors['h'].append(i)
     if not name.islower():
         print(f"File {name} is not in lowercase.")
-    if len(in_c['c']) != 0:
-        amount = len(in_c['c'])
-        print(f"{amount} comments {'has' if amount == 1 else 'have'} been detected in {name}")
-    elif len(in_c['h']) != 0:
-        amount = len(in_c['h'])
-        print(f"{amount} invalid comments {'has' if amount == 1 else 'have'} been detected in header {name}")
+    if not lines[-1][-1] == '\n':
+        print(f"File {name} doesn't have a line break at end of file.")
+    if len(errors['pathes']):
+        amount = len(errors['pathes'])
+        print(f"{amount} {'path has' if amount == 1 else 'pathes have'} been detected in {name}.")
+    if len(errors['c']) != 0:
+        amount = len(errors['c'])
+        print(f"{amount} comments {'has' if amount == 1 else 'have'} been detected in {name}.")
+    elif len(errors['h']) != 0:
+        amount = len(errors['h'])
+        print(f"{amount} invalid comments {'has' if amount == 1 else 'have'} been detected in header {name}.")
 
 
 for filename in filenames:
     relative = filename.relative_to(relativity)
     with open(filename) as file:
-        lines = [line.rstrip() for line in file]
+        lines = [line for line in file]
     manage_comments(str(relative), lines)

e900223ec77e90622d5506714f249c35d2ca5c22
Mathis Le Bonniec <mathis.le-bonniec@epitech.eu>
2022-04-29 17:56:36 +0200

    feat(utils): add utils function to get the color at a specified position in the areas image

--- src/map/get_pixel_at_pos.c
+++ src/map/get_pixel_at_pos.c
@@ -24,11 +24,8 @@ sfColor get_pixel_at_pos(game_t *game, sfVector2f shift)
     sfColor color = sfBlack;
     sfVector2u unsigned_pos = {(unsigned int) shift.x, (unsigned int) shift.y};
 
-    printf("x: %u, y: %u\n", unsigned_pos.x, unsigned_pos.y);
     if (!game || !game->collisions || is_pixel_outside_map(shift))
         return color;
-    // printf("x: %u, y: %u\n", unsigned_pos.x, unsigned_pos.y);
-    printf("pass\n");
     color = sfImage_getPixel(game->collisions, unsigned_pos.x, unsigned_pos.y);
     return color;
 }

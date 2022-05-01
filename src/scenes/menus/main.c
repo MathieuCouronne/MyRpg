56e20dc4cebce5d42f64dd1895b443146eebe8e6
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 <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>
2022-05-01 14:52:05 +0200

    feat(menu): handle close event

--- src/scenes/menus/main.c
+++ src/scenes/menus/main.c
@@ -12,6 +12,8 @@
 static void event_handling(game_t *game)
 {
     while (sfRenderWindow_pollEvent(game->window, &game->event)) {
+        if (game->event.type == sfEvtClosed)
+            sfRenderWindow_close(game->window);
         if (handle_buttons_clicks(game, game->scenes->main_menu->buttons))
             return;
     }

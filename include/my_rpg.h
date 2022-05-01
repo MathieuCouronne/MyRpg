f9ccf33bd88e1ef2df5fb4b28185c9bfe81dba5c
jtual <joshua.tual@epitech.eu>
2022-05-01 17:31:42 +0200

    feat(inventory): if item already exists, change value only

--- include/my_rpg.h
+++ include/my_rpg.h
@@ -80,7 +80,8 @@ void clock_player_right(sfClock *clock, game_t *game);
 inventory_t *create_inventory(unsigned int height, unsigned int width);
 inventory_t *add_to_inventory(inventory_t *inventory, char const *name,
     unsigned int quantity);
-inventory_t *remove_from_inventory(inventory_t *inventory, char const *name);
+inventory_t *remove_from_inventory(inventory_t *inventory, char const *name,
+    unsigned int quantity);
 inventory_t *swap_slots(inventory_t *inventory, sfVector2u pos1,
     sfVector2u pos2);
 

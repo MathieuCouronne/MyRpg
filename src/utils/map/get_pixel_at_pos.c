ba77da81158d8e702059254255707433a5a7d820
RaphaelLecoq <raphael.lecoq@epitech.eu>
2022-05-01 18:10:23 +0200

    Refactor(utils): refactor utils

---
+++ src/utils/map/get_pixel_at_pos.c
@@ -0,0 +1,31 @@
+/*
+** EPITECH PROJECT, 2022
+** get_pixel_at_pos.c
+** File description:
+** Get pixel at a specified position
+*/
+
+#include <SFML/Graphics.h>
+#include "my_rpg.h"
+#include "macros.h"
+
+static bool is_pixel_outside_map(sfVector2f shift)
+{
+    return (
+        shift.x < 0 ||
+        shift.y < 0 ||
+        shift.x > MAP_WIDTH ||
+        shift.x > MAP_HEIGHT
+    );
+}
+
+sfColor get_pixel_at_pos(game_t *game, sfVector2f shift)
+{
+    sfColor color = sfBlack;
+    sfVector2u unsigned_pos = {(unsigned int) shift.x, (unsigned int) shift.y};
+
+    if (!game || !game->collisions || is_pixel_outside_map(shift))
+        return color;
+    color = sfImage_getPixel(game->collisions, unsigned_pos.x, unsigned_pos.y);
+    return color;
+}

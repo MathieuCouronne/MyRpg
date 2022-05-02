a493f3d5441980c80809e94d36504e295e151bf2
Mathis Le Bonniec <mathis.le-bonniec@epitech.eu>
2022-05-02 16:14:33 +0200

    refactor(inventory): update inventory storage mode to use ids instead of names

--- src/controllers/inventory/add_to_inventory.c
+++ src/controllers/inventory/add_to_inventory.c
@@ -14,13 +14,13 @@
 #include "my_rpg.h"
 #include "inventory.h"
 
-static void add_to_empty_slot(slot_t **content, char const *name,
-    unsigned int quantity, unsigned int x)
+static void add_to_empty_slot(slot_t **content, unsigned int id,
+unsigned int quantity, unsigned int x)
 {
     unsigned int i = 0;
 
-    for (; my_strcmp(content[i]->name, "empty") != 0; i++);
-    content[i]->name = name;
+    for (; content[i]->id != 0 && i < INVENTORY_WIDTH; i++);
+    content[i]->id = id;
     content[i]->quantity = quantity;
     content[i]->pos.x = x;
     content[i]->pos.y = i;
@@ -29,42 +29,42 @@ static void add_to_empty_slot(slot_t **content, char const *name,
 static bool line_is_full(slot_t **content)
 {
     for (unsigned int i = 0; content[i]; i++) {
-        if (my_strcmp(content[i]->name, "empty") == 0)
+        if (content[i]->id == 0)
             return false;
     }
     return true;
 }
 
-static bool item_found(slot_t **content, char const *name)
+static bool item_found(slot_t **content, unsigned int id)
 {
     for (unsigned int i = 0; content[i]; i++) {
-        if (my_strcmp(content[i]->name, name) == 0)
+        if (content[i]->id == id)
             return true;
     }
     return false;
 }
 
-static void add_quantity(slot_t **content, char const *name,
-    unsigned int quantity)
+static void add_quantity(slot_t **content, unsigned int id,
+unsigned int quantity)
 {
     unsigned int i = 0;
 
-    for (; my_strcmp(content[i]->name, name) != 0; i++);
+    for (; content[i]->id != id; i++);
     content[i]->quantity += quantity;
 }
 
-inventory_t *add_to_inventory(inventory_t *inventory, char const *name,
-    unsigned int quantity)
+inventory_t *add_to_inventory(inventory_t *inventory, unsigned int id,
+unsigned int quantity)
 {
     for (unsigned int i = 0; inventory->content[i]; i++) {
-        if (item_found(inventory->content[i], name)) {
-            add_quantity(inventory->content[i], name, quantity);
+        if (item_found(inventory->content[i], id)) {
+            add_quantity(inventory->content[i], id, quantity);
             return inventory;
         }
     }
     for (unsigned int i = 0; inventory->content[i]; i++) {
         if (!line_is_full(inventory->content[i]))
-            add_to_empty_slot(inventory->content[i], name, quantity, i);
+            add_to_empty_slot(inventory->content[i], id, quantity, i);
     }
     return inventory;
 }

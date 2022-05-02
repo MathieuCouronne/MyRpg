d827a95fbe612c5ea7a6349c1c4b6ca5963b6d77
Mathis Le Bonniec <mathis.le-bonniec@epitech.eu>
2022-05-02 19:01:17 +0200

    refactor(assets): stop assets download when the first assets failed

--- include/messages.h
+++ include/messages.h
@@ -12,8 +12,9 @@
     #define FTP_NO_CONNECTION "Can't download assets. Check your connection.\n"
     #define FTP_WRONG_IDS "Can't download assets. Contact an admin.\n"
     #define FTP_STARTS_DOWNLOAD "Starts downloading assets from folder '%s'.\n"
-    #define FTP_DOWNLOAD_FAILED "Download failed.\n"
     #define FTP_DOWNLOAD_SUCCESSFUL "Download successful.\n"
     #define FTP_FILE_DOWNLOADED "\tAssets '%s' downloaded.\n"
+    #define FTP_FILE_DOWNLOAD_FAILED "\tAssets '%s' download failed.\n"
+    #define FTP_DOWNLOAD_FAILED "Download failed.\n"
 
 #endif // RPG_MESSAGES_H
--- src/init/assets.c
+++ src/init/assets.c
@@ -24,45 +24,54 @@ static int display_status(char *str, int status)
     return status;
 }
 
-static char *concat(char *str2)
+static char *concat(char *str)
 {
-    char *str1 = "assets/";
-    size_t len = my_strlen(str1) + my_strlen(str2);
+    char *base = "assets/";
+    size_t len = my_strlen(base) + my_strlen(str);
     char *new = malloc(sizeof(char) * (len + 1));
     size_t pos = 0;
 
     if (!new)
         return NULL;
-    for (size_t i = 0; str1[i] != '\0'; i++)
-        new[pos++] = str1[i];
-    for (size_t i = 0; str2[i] != '\0'; i++)
-        new[pos++] = str2[i];
+    for (size_t i = 0; base[i] != '\0'; i++)
+        new[pos++] = base[i];
+    for (size_t i = 0; str[i] != '\0'; i++)
+        new[pos++] = str[i];
     new[len] = '\0';
     return new;
 }
 
-static bool download_folder(sfFtp *ftp, char *folder)
+static bool download_file(sfFtp *ftp, char *folder, sfFtpListingResponse *list,
+size_t i)
 {
     sfFtpResponse *res = NULL;
+    char const *name = sfFtpListingResponse_getName(list, i);
+
+    if (!name)
+        return display_status(FTP_FILE_DOWNLOAD_FAILED, 0);
+    res = sfFtp_download(ftp, name, concat(folder), sfFtpBinary);
+    if (!sfFtpResponse_isOk(res)) {
+        my_printf(FTP_FILE_DOWNLOAD_FAILED, name);
+        return false;
+    }
+    my_printf(FTP_FILE_DOWNLOADED, name);
+    return true;
+}
+
+static bool download_folder(sfFtp *ftp, char *folder)
+{
     sfFtpListingResponse *list = sfFtp_getDirectoryListing(ftp, concat(folder));
     size_t size = 0;
-    char *dest_path = NULL;
 
     my_printf(FTP_STARTS_DOWNLOAD, folder);
     if (!sfFtpListingResponse_isOk(list))
         return display_status(FTP_WRONG_IDS, 0);
     size = sfFtpListingResponse_getCount(list);
     for (size_t i = 0; i < size; i++) {
-        char const *name = sfFtpListingResponse_getName(list, i);
-        if (!name)
-            return display_status(FTP_DOWNLOAD_FAILED, 0);
-        dest_path = concat(folder);
-        res = sfFtp_download(ftp, name, dest_path, sfFtpBinary);
-        if (!sfFtpResponse_isOk(res))
-            return display_status(FTP_DOWNLOAD_FAILED, 0);
-        my_printf(FTP_FILE_DOWNLOADED, name);
+        if (!download_file(ftp, folder, list, i))
+            return display_status(FTP_DOWNLOAD_FAILED, false);
     }
-    return display_status(FTP_DOWNLOAD_SUCCESSFUL, 1);
+    return display_status(FTP_DOWNLOAD_SUCCESSFUL, true);
 }
 
 bool download_assets(void)
@@ -70,13 +79,20 @@ bool download_assets(void)
     sfIpAddress ip = (sfIpAddress) {"185.31.40.91"};
     sfFtp *ftp = sfFtp_create();
     sfFtpResponse *res = sfFtp_connect(ftp, ip, 21, sfSeconds(5));
+    bool status = true;
 
     if (!sfFtpResponse_isOk(res))
         return display_status(FTP_NO_CONNECTION, false);
     res = sfFtp_login(ftp, "rpg", "rpg_password");
     if (!sfFtpResponse_isOk(res))
         return display_status(FTP_WRONG_IDS, false);
-    for (unsigned short i = 0; directories[i]; i++)
-        download_folder(ftp, directories[i]);
-    return true;
+    for (unsigned short i = 0; directories[i]; i++) {
+        if (!download_folder(ftp, directories[i])) {
+            status = false;
+            break;
+        }
+    }
+    if (ftp)
+        sfFtp_disconnect(ftp);
+    return status;
 }

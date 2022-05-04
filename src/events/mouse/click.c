51d36d2979b694fcb0048258823405eda1f1080d
Mathis Le Bonniec <mathis.le-bonniec@epitech.eu>
2022-05-04 16:20:11 +0200

    fix(handling): prevent errors when buttons doesn't exists

--- src/events/mouse/click.c
+++ src/events/mouse/click.c
@@ -11,7 +11,7 @@ bool handle_buttons_clicks(game_t *game, button_t **buttons)
 {
     sfVector2i pos = {game->event.mouseButton.x, game->event.mouseButton.y};
 
-    if (game->event.type != sfEvtMouseButtonPressed)
+    if (game->event.type != sfEvtMouseButtonPressed || !buttons)
         return false;
     for (unsigned short i = 0; buttons[i]; i++) {
         if (!is_pos_in_button(buttons[i], pos))

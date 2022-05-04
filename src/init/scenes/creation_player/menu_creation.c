9af7902483b3f2a07f343065576e39448be6af27
Mathieu Couronne <mathieu.couronne@epitech.eu>
2022-05-04 09:41:10 +0200

    feat(creation): add diredctory init creation player

---
+++ src/init/scenes/creation_player/menu_creation.c
@@ -0,0 +1,38 @@
+/*
+** EPITECH PROJECT, 2021
+** game.c
+** File description:
+** Game initializer
+*/
+
+#include <SFML/Graphics.h>
+#include <SFML/Window.h>
+#include <stdlib.h>
+#include "menu.h"
+#include "structs.h"
+#include "my_rpg.h"
+#include "macros.h"
+
+game_asset_t *init_creation_background(game_t *game)
+{
+    game_asset_t *background = malloc(sizeof(game_asset_t));
+
+    if (!background)
+        return NULL;
+    background->sprite = sfSprite_create();
+    background->texture = sfTexture_createFromFile(PAUSE_BG_PATH, NULL);
+    sfSprite_setTexture(background->sprite, background->texture, sfTrue);
+    return background;
+}
+
+main_creation_scenes_t *init_creation(game_t *game)
+{
+    pause_t *scene = malloc(sizeof(pause_t));
+
+    if (!scene)
+        return NULL;
+    scene->background = init_pause_background(game);
+    scene->buttons = init_buttons_pause();
+    return scene;
+}
+

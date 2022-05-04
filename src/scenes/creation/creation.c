be489d0b0e4482046d07dd783e3e923bacfbf180
Mathieu Couronne <mathieu.couronne@epitech.eu>
2022-05-04 20:27:48 +0200

    style(creation): remove to long line

--- src/scenes/creation/creation.c
+++ src/scenes/creation/creation.c
@@ -2,14 +2,12 @@
 ** EPITECH PROJECT, 2021
 ** handling.c
 ** File description:
-** Game arrow keys events handling
+** create creation player scene
 */
 
 #include <stdbool.h>
 #include "my_rpg.h"
 #include "structs.h"
-
-extern const stats_t class_stats[];
 
 static void event_handling(game_t *game)
 {
@@ -18,32 +16,37 @@ static void event_handling(game_t *game)
     }
 }
 
-void display_buttons_creation(sfRenderWindow *window,
+void display_buttons_creation(sfRenderWindow *w,
 main_creation_scenes_t *creation)
 {
-    sfRenderWindow_drawSprite(window, creation->buttons[0]->asset->sprite, NULL);
-    sfRenderWindow_drawSprite(window, creation->buttons[1]->asset->sprite, NULL);
-    sfRenderWindow_drawSprite(window, creation->buttons[2]->asset->sprite, NULL);
-    sfRenderWindow_drawSprite(window, creation->buttons[3]->asset->sprite, NULL);
-    sfRenderWindow_drawSprite(window, creation->buttons[4]->asset->sprite, NULL);
-    sfRenderWindow_drawSprite(window, creation->buttons[5]->asset->sprite, NULL);
-    sfRenderWindow_drawSprite(window, creation->buttons[6]->asset->sprite, NULL);
-    sfRenderWindow_drawSprite(window, creation->buttons[7]->asset->sprite, NULL);
-    sfRenderWindow_drawSprite(window, creation->buttons[8]->asset->sprite, NULL);
-    sfRenderWindow_drawSprite(window, creation->buttons[9]->asset->sprite, NULL);
-    sfRenderWindow_drawSprite(window, creation->buttons[10]->asset->sprite, NULL);
-    sfRenderWindow_drawSprite(window, creation->buttons[11]->asset->sprite, NULL);
+    sfRenderWindow_drawSprite(w, creation->buttons[0]->asset->sprite, NULL);
+    sfRenderWindow_drawSprite(w, creation->buttons[1]->asset->sprite, NULL);
+    sfRenderWindow_drawSprite(w, creation->buttons[2]->asset->sprite, NULL);
+    sfRenderWindow_drawSprite(w, creation->buttons[3]->asset->sprite, NULL);
+    sfRenderWindow_drawSprite(w, creation->buttons[4]->asset->sprite, NULL);
+    sfRenderWindow_drawSprite(w, creation->buttons[5]->asset->sprite, NULL);
+    sfRenderWindow_drawSprite(w, creation->buttons[6]->asset->sprite, NULL);
+    sfRenderWindow_drawSprite(w, creation->buttons[7]->asset->sprite, NULL);
+    sfRenderWindow_drawSprite(w, creation->buttons[8]->asset->sprite, NULL);
+    sfRenderWindow_drawSprite(w, creation->buttons[9]->asset->sprite, NULL);
+    sfRenderWindow_drawSprite(w, creation->buttons[10]->asset->sprite, NULL);
+    sfRenderWindow_drawSprite(w, creation->buttons[11]->asset->sprite, NULL);
 }
 
 void display_stat(main_creation_scenes_t *creation,
 sfRenderWindow *window)
 {
     for (size_t i = 0; i < 5; i++) {
-        sfRenderWindow_drawText(window, creation->stat[creation->class][0], NULL);
-        sfRenderWindow_drawText(window, creation->stat[creation->class][1], NULL);
-        sfRenderWindow_drawText(window, creation->stat[creation->class][2], NULL);
-        sfRenderWindow_drawText(window, creation->stat[creation->class][3], NULL);
-        sfRenderWindow_drawText(window, creation->stat[creation->class][4], NULL);
+        sfRenderWindow_drawText(window,
+    creation->stat[creation->class][0], NULL);
+        sfRenderWindow_drawText(window,
+    creation->stat[creation->class][1], NULL);
+        sfRenderWindow_drawText(window,
+    creation->stat[creation->class][2], NULL);
+        sfRenderWindow_drawText(window,
+    creation->stat[creation->class][3], NULL);
+        sfRenderWindow_drawText(window,
+    creation->stat[creation->class][4], NULL);
     }
 }
 

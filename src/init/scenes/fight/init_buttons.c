8fc96b243bf3d437a8e21ec85a2d9d3208c6ead7
jtual <joshua.tual@epitech.eu>
2022-05-04 13:08:49 +0200

    feat(fight_buttons): created buttons

---
+++ src/init/scenes/fight/init_buttons.c
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2021
+** init_menu_button.c
+** File description:
+** init menu button
+*/
+
+#include <stdlib.h>
+#include <SFML/Window.h>
+#include "structs.h"
+#include "my_rpg.h"
+
+button_t **init_attack_buttons(void)
+{
+    button_t **buttons = malloc(sizeof(button_t *) * 4);
+    float pos_x = 960 - (float) 390 / 2;
+    sfVector2f scale = {1, 1};
+
+    if (!buttons)
+        return NULL;
+    buttons[0] = create_button("Mathis", (sfVector2f) {pos_x, 500}, go_to_saves,
+        scale);
+    buttons[1] = create_button("Le",(sfVector2f) {pos_x, 650},
+        go_to_settings, scale);
+    buttons[2] = create_button("Bouffon", (sfVector2f) {pos_x, 800}, quit_game,
+        scale);
+    buttons[3] = NULL;
+    return buttons;
+}

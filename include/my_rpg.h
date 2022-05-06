b0b40aeaedd7ad3d29b606b9bd53633d397422c9
RaphaelLecoq <raphael.lecoq@epitech.eu>
2022-05-06 10:21:46 +0200

    feat(fight): text_fight

--- include/my_rpg.h
+++ include/my_rpg.h
@@ -63,6 +63,7 @@ loader_t *init_loader_window(int *total);
 sfText ***create_stat_text(sfFont *font, main_creation_scenes_t *creation);
 stats_t *init_war_stats(void);
 sfText **init_unspent(main_creation_scenes_t *creation, sfFont *font);
+sfText **init_text_life(fight_t *text_fight);
 
 // Destroy
 void destroy_game(game_t *game);

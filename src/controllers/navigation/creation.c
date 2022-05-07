7ddc80ed0e294a0effbc82f29750f7097d693811
Mathieu Couronne <mathieu.couronne@epitech.eu>
2022-05-07 15:12:49 +0200

    fix(view): fix view when create char

--- src/controllers/navigation/creation.c
+++ src/controllers/navigation/creation.c
@@ -47,9 +47,6 @@ void create_game(game_t *game)
     game->saves[game->current]->unspent =
     game->scenes->creation_menu->unspent[game->scenes->creation_menu->class];
     game->scenes->stats = init_stats(game);
-    game->view = sfView_createFromRect(view_rect);
     sfView_setCenter(game->view, (sfVector2f) {2270, 2030});
-    sfView_zoom(game->view, 1.f / MAP_SCALE);
-    sfRenderWindow_setView(game->window, game->view);
     game->scenes->current = MAIN_GAME;
 }

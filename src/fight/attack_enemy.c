98d6c0cf48ae284a3997594d56d7d72ed614fb0f
jtual <joshua.tual@epitech.eu>
2022-05-07 17:57:48 +0200

    style: fix line length

--- src/fight/attack_enemy.c
+++ src/fight/attack_enemy.c
@@ -15,7 +15,9 @@ void attack_enemy(game_t *game)
     unsigned int dmg = game->saves[game->current]->stats->strength;
 
     game->scenes->fight->enemy[game->enemy_id]->hp -= dmg;
-    create_text_enemy(game->scenes->fight->enemy[game->enemy_id], game->scenes->fight);
-    attack_player(game->saves[game->current], game->scenes->fight->enemy[game->enemy_id]);
+    create_text_enemy(game->scenes->fight->enemy[game->enemy_id],
+        game->scenes->fight);
+    attack_player(game->saves[game->current],
+        game->scenes->fight->enemy[game->enemy_id]);
     create_text_player(game->saves[game->current], game->scenes->fight);
 }

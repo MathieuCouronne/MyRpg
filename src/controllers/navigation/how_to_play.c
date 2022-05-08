f265fb468982a7d41409290bdc41c10448c61e95
Mathieu Couronne <mathieu.couronne@epitech.eu>
2022-05-08 19:22:20 +0200

    feat(help): add help window

---
+++ src/controllers/navigation/how_to_play.c
@@ -0,0 +1,37 @@
+/*
+** EPITECH PROJECT, 2021
+** global.c
+** File description:
+** Globals navigation functions
+*/
+
+#include "my_rpg.h"
+
+void event_handling(sfRenderWindow *window, sfEvent event)
+{
+    while (sfRenderWindow_pollEvent(window, &event)) {
+        if (event.type == sfEvtClosed)
+            sfRenderWindow_close(window);
+    }
+}
+
+void window_help(game_t *game)
+{
+    sfVideoMode mode = {1200, 700, 32};
+    sfRenderWindow *window = sfRenderWindow_create(mode, "Help", sfClose, NULL);
+    game_asset_t *help = NULL;
+    sfEvent event = {0};
+
+    if (!game || !game->help)
+        return ;
+    help = game->help;
+    sfRenderWindow_setVisible(game->window, false);
+    while (sfRenderWindow_isOpen(window)) {
+        event_handling(window, event);
+        sfRenderWindow_clear(window, sfBlack);
+        sfRenderWindow_drawSprite(window, help->sprite, NULL);
+        sfRenderWindow_display(window);
+    }
+    sfRenderWindow_destroy(window);
+    sfRenderWindow_setVisible(game->window, true);
+}
---
+++ src/init/help.c
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2021
+** inventory.c
+** File description:
+** init_inventory
+*/
+
+#include <SFML/Audio.h>
+#include <stdlib.h>
+#include "structs.h"
+#include "menu.h"
+#include "my_rpg.h"
+#include "macros.h"
+
+game_asset_t *init_help(void)
+{
+    game_asset_t *background = malloc(sizeof(game_asset_t));
+
+    if (!background)
+        return NULL;
+    background->sprite = sfSprite_create();
+    background->texture = sfTexture_createFromFile(CREATION_BG_PATH, NULL);
+    if (!background->sprite || !background->texture)
+        return NULL;
+    sfSprite_setTexture(background->sprite, background->texture, sfTrue);
+    return background;
+}

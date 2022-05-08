4a24cdfb7ae3b096ae15b7a4e28b12a692ee3a48
Mathis Le Bonniec <mathis.le-bonniec@epitech.eu>
2022-05-08 23:16:11 +0200

    fix: download credits files

--- src/init/assets.c
+++ src/init/assets.c
@@ -21,7 +21,7 @@ static char *directories[] = {
     "images/main_menu", "images/game", "images/misc",
     "images/settings", "images/pause", "images/saves",
     "images/inventory", "images/creation", "images/help",
-    "images/creation", NULL
+    "images/credits", NULL
 };
 
 static int display_status(char *str, int status)
